6f1b2a3e236eb3470a75a6791e03d0e1be6f7130
Kola Antoshkiv <ak.pkqtor@gmail.com>
2020-12-24 21:37:21 +0200

    Add third task

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
  * @returns {boolean}
 */
 
-const isAdult = function(age){
-  if(age >= ADULT_AGE){
+const isAdult = function (age) {
+  if (age >= ADULT_AGE) {
     return true;
   }
   return false;
@@ -21,8 +21,24 @@ const isAdult = function(age){
  * @param {number} multiplicityNumber
  * @returns {boolean}
 */
-const checkMultiplicity = function(checkedNumber, multiplicityNumber){
-  if(checkedNumber % multiplicityNumber === 0){
+const checkMultiplicity = function (checkedNumber, multiplicityNumber) {
+  if (checkedNumber % multiplicityNumber === 0) {
+    return true;
+  }
+  return false;
+}
+
+//task3
+
+/** 
+ * checking a triangle for existence
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @returns {boolean}
+*/
+const isTriangleReal = function (a, b, c) {
+  if(a > 0 && b > 0 && c > 0 && (a < (b+c) && b < (a+c) && c < (a+b))){
     return true;
   }
   return false;
